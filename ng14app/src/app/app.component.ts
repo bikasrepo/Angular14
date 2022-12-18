d99be0f406a5b15cafe953326adb8542e1f26d49
Bikas <talk2bikas@hotmail.com>
2022-12-18 14:26:30 +0530

    Angular 15 typed form (Angular 14 feature)

--- ng14app/src/app/app.component.ts
+++ ng14app/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng14app';
+
+  myForm: FormGroup<{
+    firstName: FormControl<string>,
+    lastName: FormControl<string>,
+    age: FormControl<any>
+  }>;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.nonNullable.group({
+      firstName: '',
+      lastName: '',
+      age: ''
+    })
+  }
+
+  ngOnInit() {
+    //this.myForm.value.
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
